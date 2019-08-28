4a58be4f059dc86c5bc55ff4892b061501b8f33a
Phillip Pense <39144472+proudtxn@users.noreply.github.com>
2019-08-28 15:47:19 -0500

    created footer in html and styled it in css

--- realtyPage/js/main.js
+++ realtyPage/js/main.js
@@ -18,3 +18,30 @@ function fixNav() {
 
 window.addEventListener('scroll', fixNav);
 // END ~ MAKING NAVBAR STICKY & ADJUSTING SOME OF ITS PROPERTIES
+
+
+//  BURGER FUNCTIONALITY
+const navSlide = () => {
+    const burger = document.querySelector('.nav-btn');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    
+    burger.addEventListener('click', () => {
+
+        // Toggle Nav Slide
+        nav.classList.toggle('nav-active');
+
+        // Animate 'a'Links Slide in
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            }
+            else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 15 + 0.5}s`;
+            }
+        });
+    });
+}
+navSlide();
+//  END ~ BURGER FUNCTIONALITY
